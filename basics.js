4af6a27a8f8a584ac15961f92ec2b918491d92ac
imjayapple <imjayapple@gmail.com>
2023-11-21 13:59:03 -0800

    Basics of JS, and Data Types

--- basics.js
+++ basics.js
@@ -1,1 +1,11 @@
+// .log() method, when we write console.log() whatever we put inside the parenthesis gets logged to the console
+console.log(5);
+console.log(6);
 
+// Two ways to comment: //
+
+/*
+This is all commented
+and this as well 
+and finally this too
+*/
